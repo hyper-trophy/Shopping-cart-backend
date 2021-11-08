3c9bbe4c2b053058ae4ce02085188e6ee9945eea
Anupam <aannupppaamm11@gmail.com>
2021-11-08 16:57:23 +0530

    created barcode API

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+
+from database.services import add_to_cart
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     return 'Hello, World!'
+
 @app.route('/')
-def index():
-    return 'Hello, World!'
+def barcode():
+    try:
+        product_id = request.args.get('barcode')
+        print("id: "+str(product_id))
+        add_to_cart(product_id)
+        return 'OK'
+    except:
+        return 'transaction unsuccessful'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from database.services import *
 from database.base import Base, engine, Session
 Base.metadata.create_all(engine)
-add_to_cart(2)
+add_to_cart(10)
